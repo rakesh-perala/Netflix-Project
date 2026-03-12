386f98eff84022cd97d36b9021c2aea88c8c17e3
Rakesh Perala <raakeshperala16@gmail.com>
2026-03-12 10:25:39 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-buketrk" # Replace with your actual S3 bucket name
+    bucket = "devopsfactory-bk" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-2"
   }
